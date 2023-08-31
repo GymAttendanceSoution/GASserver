d6d0ebebe675df0c51f989ebc078aebac2f3abd2
chansoo <chansoo38@gmail.com>
2023-08-31 20:25:33 +0900

    [feat] : 프로젝트 생성

---
+++ src/test/java/gymProgect/guardPass/GuardPassApplicationTests.java
@@ -0,0 +1,13 @@
+package gymProgect.guardPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
